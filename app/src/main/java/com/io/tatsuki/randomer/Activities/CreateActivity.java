29e5d3d5840223f7c03d4b5d69cce7750d6ecf5f
TatsukiIshijima <tatsuki445@gmail.com>
2017-08-02 20:57:51 +0900

    画面ごとのActivity作成

---
+++ app/src/main/java/com/io/tatsuki/randomer/Activities/CreateActivity.java
@@ -0,0 +1,15 @@
+package com.io.tatsuki.randomer.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.io.tatsuki.randomer.R;
+
+public class CreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create);
+    }
+}
