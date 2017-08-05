425b949b77f41fc3be41c5339e8352fbb38e5896
TatsukiIshijima <tatsuki445@gmail.com>
2017-08-05 16:21:19 +0900

    ItemAdapter作成

--- app/src/main/java/com/io/tatsuki/randomer/Adapters/ItemAdapter.java
+++ app/src/main/java/com/io/tatsuki/randomer/Adapters/ItemAdapter.java
@@ -1,11 +1,18 @@
 package com.io.tatsuki.randomer.Adapters;
 
+import android.content.Context;
+import android.databinding.DataBindingUtil;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.io.tatsuki.randomer.Models.Item;
+import com.io.tatsuki.randomer.R;
 import com.io.tatsuki.randomer.ViewModels.ItemViewModel;
+import com.io.tatsuki.randomer.databinding.ItemBinding;
+
+import java.util.ArrayList;
 
 /**
  * ホーム画面のリストアダプター
@@ -13,19 +20,45 @@ import com.io.tatsuki.randomer.ViewModels.ItemViewModel;
 
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder>{
 
+    private static final String TAG = ItemAdapter.class.getSimpleName();
+    private Context mContext;
+    private ItemBinding mBinding;
+    private ArrayList<Item> mItems;
+
+    /**
+     * コンストラクタ
+     * @param context
+     * @param items
+     */
+    public ItemAdapter(Context context , ArrayList<Item> items) {
+        this.mContext = context;
+        this.mItems = items;
+    }
+
     @Override
     public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        mBinding = DataBindingUtil.inflate(LayoutInflater.from(mContext), R.layout.item, parent, false);
+        mBinding.setItemViewModel(new ItemViewModel());
+        return new ItemViewHolder(mBinding.getRoot(), mBinding.getItemViewModel());
     }
 
     @Override
     public void onBindViewHolder(ItemViewHolder holder, int position) {
-
+        Item item = getItemAt(holder.getLayoutPosition());
+        holder.loadModel(item);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        if (mItems == null) {
+            return 0;
+        } else {
+            return mItems.size();
+        }
+    }
+
+    private Item getItemAt(int position) {
+        return mItems.get(position);
     }
 
     /**
