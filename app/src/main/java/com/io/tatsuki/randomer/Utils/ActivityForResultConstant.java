73057a9d8912dda430b342ef2bf6c2b35b3ab438
TatsukiIshijima <tatsuki445@gmail.com>
2017-08-13 14:46:47 +0900

    定数群の追加

--- app/src/main/java/com/io/tatsuki/randomer/Utils/ActivityForResultConstant.java
+++ app/src/main/java/com/io/tatsuki/randomer/Utils/ActivityForResultConstant.java
@@ -6,8 +6,11 @@ package com.io.tatsuki.randomer.Utils;
 
 public class ActivityForResultConstant {
 
-    public static final int ACTIVITY_REQUEST_CODE    = 1;
-    public static final int DELETE_REQUEST           = 2;
-    public static final int REGISTER_REQUEST         = 3;
-    public static final String DELETE_MESSAGE = "DELETE_MESSAGE";
+    public static final int ACTIVITY_REQUEST_CODE    = 1;       //
+    public static final int DELETE_REQUEST           = 2;       // 削除リクエスト
+    public static final int REGISTER_REQUEST         = 3;       // 登録リクエスト
+    public static final int EDIT_REQUEST             = 4;       // 編集リクエスト
+    public static final String DELETE_MESSAGE        = "DELETE_MESSAGE";
+    public static final String REGISTER_MESSAGE      = "REGISTER_MESSAGE";
+    public static final String EDIT_MESSAGE          = "EDIT_MESSAGE";
 }
