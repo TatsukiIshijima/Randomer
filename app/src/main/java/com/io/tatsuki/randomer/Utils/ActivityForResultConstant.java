65e98af79cc3b044e9050d3c8f931044b40fb602
TatsukiIshijima <tatsuki445@gmail.com>
2017-08-13 14:39:45 +0900

    ActivityForResultのための定数クラス作成

---
+++ app/src/main/java/com/io/tatsuki/randomer/Utils/ActivityForResultConstant.java
@@ -0,0 +1,13 @@
+package com.io.tatsuki.randomer.Utils;
+
+/**
+ * ActivityForResultで使用する定数群
+ */
+
+public class ActivityForResultConstant {
+
+    public static final int ACTIVITY_REQUEST_CODE    = 1;
+    public static final int DELETE_REQUEST           = 2;
+    public static final int REGISTER_REQUEST         = 3;
+    public static final String DELETE_MESSAGE = "DELETE_MESSAGE";
+}
