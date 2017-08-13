e46ab8072c11029b63fb9cc8e69301ed657e2189
TatsukiIshijima <tatsuki445@gmail.com>
2017-08-13 15:38:56 +0900

    変数名変更

--- app/src/main/java/com/io/tatsuki/randomer/Utils/ActivityForResultConstant.java
+++ app/src/main/java/com/io/tatsuki/randomer/Utils/ActivityForResultConstant.java
@@ -9,7 +9,7 @@ public class ActivityForResultConstant {
     public static final int ACTIVITY_REQUEST_CODE    = 1;       //
     public static final int DELETE_REQUEST           = 2;       // 削除リクエスト
     public static final int REGISTER_REQUEST         = 3;       // 登録リクエスト
-    public static final int EDIT_REQUEST             = 4;       // 編集リクエスト
+    public static final int SAVE_REQUEST             = 4;       // 保存リクエスト
     public static final String DELETE_MESSAGE        = "DELETE_MESSAGE";
     public static final String REGISTER_MESSAGE      = "REGISTER_MESSAGE";
     public static final String EDIT_MESSAGE          = "EDIT_MESSAGE";
