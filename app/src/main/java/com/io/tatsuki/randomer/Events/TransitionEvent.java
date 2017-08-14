15704e9613ddd07409d7545641e693d41f8a0f91
TatsukiIshijima <tatsuki445@gmail.com>
2017-08-14 10:32:09 +0900

    フラグの追加

--- app/src/main/java/com/io/tatsuki/randomer/Events/TransitionEvent.java
+++ app/src/main/java/com/io/tatsuki/randomer/Events/TransitionEvent.java
@@ -10,9 +10,10 @@ import lombok.Getter;
 
 public class TransitionEvent {
 
-    public static final int TRANS_TO_REGISTER_FLAG = 1;         // 登録画面遷移用フラグ
-    public static final int TRANS_TO_DETAIL_FLAG   = 2;         // 詳細画面遷移用フラグ
-    public static final int TRANS_TO_HOME_FLAG     = 3;         // ホーム画面遷移用フラグ
+    public static final int TRANS_TO_REGISTER_FLAG                = 1;         // 登録画面遷移用フラグ
+    public static final int TRANS_TO_DETAIL_FLAG                  = 2;         // 詳細画面遷移用フラグ
+    public static final int TRANS_TO_HOME_FLAG                    = 3;         // ホーム画面遷移用フラグ
+    public static final int BACK_CATEGORY_SELECT_TO_REGISTER_FLAG = 4;         // カテゴリーアイコン選択から登録画面に戻る用のフラグ
 
     @Getter
     private int transitionFlag;
